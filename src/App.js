1484d3730e6f6136f4968c3ed8e72006a4fd4f86
Bryenne Kay <bryenne.k@gmail.com>
2020-01-14 21:33:44 +0900

    back to proken progress bar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import "./CircularProgressbar.js";
 import 'react-circular-progressbar/dist/styles.css';
 import { getPercentageDone } from "./GetPercentageDone.js";
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
-import { CircularProgressbarf } from "./CircularProgressbar.js";
 
 function CountdownTimer() {
   const calculateTimeLeft = () => {
@@ -54,7 +53,35 @@ function CountdownTimer() {
     <div class="app">
       <h1>Countdown to See NICK 💘 </h1>
       {timerComponents.length ? timerComponents : <span>Time To See NICK</span>}
-      <h2>You're {CircularProgressbar}
+      <h2>You're <CircularProgressbar
+  value={getPercentageDone}
+  text={`${getPercentageDone}%`}
+
+
+  styles={buildStyles({
+  // Rotation of path and trail, in number of turns (0-1)
+  rotation: 0.25,
+
+  // Whether to use rounded or flat corners on the ends - can use 'butt' or 'round'
+  strokeLinecap: 'butt',
+
+  // Text size
+  textSize: '16px',
+
+  // How long animation takes to go from one percentage to another, in seconds
+  pathTransitionDuration: 0.5,
+
+  // Can specify path transition in more detail, or remove it entirely
+  pathTransition: 'none',
+
+  // Colors
+  pathColor: `rgba(62, 152, 199, ${getPercentageDone})`,
+  textColor: 'red',
+  trailColor: '#d6d6d6',
+  backgroundColor: '#3e98c7',
+  })}
+  />
+
  of the way there!</h2>
     </div>
     );
